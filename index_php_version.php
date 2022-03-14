c56afb8565463d6f3b93c22df3f2b8b0e78c4200
pietroline <pietrobarberi@hotmail.it>
2022-03-14 17:08:34 +0100

    milestone 1

--- index_php_version.php
+++ index_php_version.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__ . "/database.php";
+    require_once __DIR__ . "/src/database.php";
 ?>
 
 
@@ -12,8 +12,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Dischi php version</title>
 
-        <link rel="stylesheet" href="style.css">
+        <!-- link style css -->
+        <link rel="stylesheet" href="css/style.css">
+
+        <!-- link icone bootstrap -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+        <!-- link cdn boottrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     </head>
