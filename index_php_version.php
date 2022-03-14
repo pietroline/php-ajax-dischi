3d6d7600206e352494ce5c7deb5670d852a50cf1
pietroline <pietrobarberi@hotmail.it>
2022-03-14 16:38:47 +0100

    milestone 1

--- index_php_version.php
+++ index_php_version.php
@@ -1,3 +1,8 @@
+<?php
+    require_once __DIR__ . "/database.php";
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,22 +13,49 @@
         <title>Dischi php version</title>
 
         <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     </head>
 
     <body>
 
-
-        <header class="ms_d-flex ms_align_items_center">
-            <i class="fab fa-spotify"></i>
+        <header>
+            <div class="row">
+                <div class="col">
+                    <i class="bi bi-spotify"></i>
+                </div>    
+            </div>
         </header>
 
-        <main>
-            
+        <main class="p-5">
+            <div class="container">
+                <div class="row row-cols-2  row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
+
+                    <?php
+                        foreach($database as $disco){
+                            echo "<div class='col'>";
+                                echo "<div class='album d-flex flex-column align-items-center'>";
+                                    echo "<div class='ms_w-90 pt-3'>";
+                                            echo  "<img src=$disco[poster] class='card-img-top' alt=img $disco[title]>";
+                                    echo "</div>";
+                                    echo "<div class='card-body text-center'>";
+                                        echo "<h4 class='card-title mb-4'>$disco[title]</h4>";
+                                        echo "<h6 class='text-secondary'>$disco[author]</h6>";
+                                        echo "<h6 class='text-secondary'>$disco[year]</h6>";
+                                    echo "</div>";
+                                echo "</div>";
+                            echo "</div>";
+                        } 
+                    ?>
+                      
+                </div>
+                
+            </div>
         </main>
-        
-        
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>        
     </body>
 
 </html>
