c9e7cfb4a6e750e260291cc35f83c661452bb055
pietroline <pietrobarberi@hotmail.it>
2022-03-14 15:08:29 +0100

    initial commit and style web page

---
+++ index_php_version.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dischi php version</title>
+
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    </head>
+
+    <body>
+
+
+        <header class="ms_d-flex ms_align_items_center">
+            <i class="fab fa-spotify"></i>
+        </header>
+
+        <main>
+            
+        </main>
+        
+        
+    </body>
+
+</html>
