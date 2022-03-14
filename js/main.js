d24b47146d7e46b226d96bff5bcd970cd463cba4
pietroline <pietrobarberi@hotmail.it>
2022-03-14 17:50:30 +0100

    rotto sul rendering

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+
+const app = new Vue({
+    
+    el: '#app',
+    data: {
+        albums: "",
+    },
+
+    mounted(){
+        this.getInfoSong();
+    },
+
+
+    
+    methods:{
+        
+        getInfoSong(){
+            
+        
+            axios.get("http://localhost/php-ajax-dischi/src/server.php")
+            .then(function (response) {
+                this.albums = response.data;
+                console.log(this.albums)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            
+        }
+    }
+})
